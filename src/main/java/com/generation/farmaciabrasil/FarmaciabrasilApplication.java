077a2660172a1b29dd8a0364328fd050907e693e
EricaArj <erika.f55@hotmail.com>
2023-03-20 18:03:00 -0300

    Sistema Spring

---
+++ src/main/java/com/generation/farmaciabrasil/FarmaciabrasilApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciabrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciabrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciabrasilApplication.class, args);
+	}
+
+}
